8a010a252ba0d081292dea3013c873eb55b7cc94
ahmed shatat <a.shatat@hotmail.com>
2018-12-16 22:51:26 +0200

    feat add StagedAction model #11

---
+++ server/database/models/StagedAction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// StagedAction Schema
+// Defines how to store stagedActions from members against proposedGoals and projects (edit, delete)
+
+const StagedActionSchema = new Schema({
+  action: String,
+  description: String,
+  users: [{
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  }],
+  category: {
+    type: String,
+    enum: ['goals', 'projects'],
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    refPath: 'category',
+  }
+});
+
+module.exports = StagedAction = mongoose.model('stagedactions', StagedActionSchema);
