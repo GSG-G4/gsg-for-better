7ba1b635f2b1f5e612ba19e107f6ad7c2cc42ac4
ahmed shatat <a.shatat@hotmail.com>
2018-12-16 22:46:26 +0200

    feat: add db_connection setup #11

---
+++ server/database/db_connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// load .env only on development
+if (!process.env.TRAVIS || !process.env.ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const dbConnection = () => {
+  let { MONGOURI: mongoURI } = process.env;
+
+  if (process.env.NODE_ENV === 'test') {
+    // change MONGOURI to testDBurl
+    mongoURI = process.env.MONGOURI_TEST; 
+  }
+  // create DB connection
+  mongoose.connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  );
+}
+
+module.exports = dbConnection;
