0c2def936d6d9d73e23b6df8074f64f70ab840d5
ahmed shatat <a.shatat@hotmail.com>
2018-12-16 22:52:09 +0200

    feat add Project model #11

---
+++ server/database/models/Project.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Project Schema
+// Defines how to store projects info
+
+const ProjectSchema = new Schema({
+  githubLink: String,
+  projectSite: String,
+  projectStatus: String,
+  previewImage: String,
+  description: String,
+  technologies: [{
+    type: Schema.Types.ObjectId,
+    ref: 'technologies',
+  }],
+  team: {
+    type: Schema.Types.ObjectId,
+    ref: 'teams',
+  },
+  goals: [{
+    type: Schema.Types.ObjectId,
+    ref: 'goals',
+  }],
+});
+
+module.exports = Project = mongoose.model('projects', ProjectSchema);
