65e925765d61ee63ea6447dfda26bb2ba83c3bf1
amusameh <abdalsamad.y.m@gmail.com>
2018-12-18 00:45:34 +0200

    chore: Add eslint configerations relates #14

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    // disables the windows/unix linebreak checks.
+    "linebreak-style": 0,
+    "eslint linebreak-style": [0, "error", "windows"]
+  }
+};
