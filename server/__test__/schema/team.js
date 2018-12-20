7fb552478bd3279c8e9374daf532f44f3cbf044a
ahmed shatat <a.shatat@hotmail.com>
2018-12-20 23:03:29 +0200

    feat: add team schema test #66

---
+++ server/__test__/schema/team.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const buildFakeData = require('../../database/fake_data');
+const Team = require('../../database/models/team');
+
+describe('testing for Team schema', () => {
+  beforeEach(async () => {
+    await buildFakeData();
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  test('Team schema must be defined', async () => {
+    // Team schema must be defined
+    expect(Team).toBeDefined();
+
+    // find all teams
+    const teams = await Team.find({});
+
+    // expect to get array of object
+    expect(teams).toBeDefined();
+    expect(Array.isArray(teams)).toBeTruthy();
+
+    // expect to get 3 teams [G4-AFAR, ILA, KMA]
+    expect(teams.length).toBe(3);
+
+    // testing for collections names
+    expect(teams[0]).toMatchObject({ name: 'G4-AFAR' });
+    expect(teams[1]).toMatchObject({ name: 'ILA' });
+    expect(teams[2]).toMatchObject({ name: 'KMA' });
+  });
+});
