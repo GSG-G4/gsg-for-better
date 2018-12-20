3b148bc97944327a8482f8d9b68123a35290b28f
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-20 13:58:29 +0200

    feat: reset User collection in delete_fake_data Relates #67

--- server/database/fake_data/delete_fake_data.js
+++ server/database/fake_data/delete_fake_data.js
@@ -1,7 +1,9 @@
 const Technology = require('../models/technology');
+const User = require('../models/user');
 
 const resetFakeData = async () => {
   await Technology.deleteMany();
+  await User.deleteMany();
 };
 
 module.exports = resetFakeData;
