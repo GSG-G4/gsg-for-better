819251ca6f877197529704372cbc13bb6a8ee667
FarahZaqout <farah.zaqout@gmail.com>
2018-12-21 23:31:57 +0200

    adds test cases to the project schema
    relates #71

---
+++ server/__test__/schema/project.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const buildFakeData = require('../../database/fake_data');
+const Project = require('../../database/models/project');
+
+describe('testing project model', () => {
+  beforeEach(async () => {
+    await buildFakeData();
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  test('project schema interacts as intended', async () => {
+    const projects = await Project.find({});
+    expect(projects).toBeDefined();   
+    // progress has 6 entries
+    expect(projects.length).toBe(3);
+    expect(projects[0].projectStatus).toBe('Done');
+    expect(projects[2].projectStatus).toBe('In progress');
+  });
+});
