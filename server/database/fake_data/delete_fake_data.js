5132af82b52d1ba77709991b94333df6dee0dfa3
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-21 22:18:16 +0200

    test: insert comments fake data Rekates #68

--- server/database/fake_data/delete_fake_data.js
+++ server/database/fake_data/delete_fake_data.js
@@ -1,11 +1,13 @@
 const Technology = require('../models/technology');
 const Team = require('../models/team');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 const resetFakeData = async () => {
   await Technology.deleteMany();
   await Team.deleteMany();
   await User.deleteMany();
+  await Comment.deleteMany();
 };
 
 module.exports = resetFakeData;
--- server/database/fake_data/index.js
+++ server/database/fake_data/index.js
@@ -6,6 +6,7 @@ const resetFakeData = require('./delete_fake_data');
 const technologyFakeData = require('./technology');
 const teamFakeData = require('./team');
 const userFakeData = require('./user');
+const commentFakeData = require('./comment');
 
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 const dbConnection = require('./../db_connection');
@@ -17,6 +18,7 @@ const build = () => new Promise((resolve, reject) => {
       await technologyFakeData();
       await teamFakeData();
       await userFakeData();
+      await commentFakeData();
     })
     .then(resolve)
     .catch((err) => {
