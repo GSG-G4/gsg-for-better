13cd06563cfd659fa1d990452ee48f5a2a505496
FarahZaqout <farah.zaqout@gmail.com>
2018-12-24 18:14:16 +0200

    creates wrapper component for the Landing Page
    relates #103

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import Header from '../Header';
+import Community from './Community';
+import ProjectsSection from './ProjectsSection';
+import Team from './Team';
+import TechStack from './TechStack';
+import Timeline from './Timeline';
+import Footer from '../Footer';
+
+export default function() {
+  return (
+    <Fragment>
+      <Header />
+      <Community />
+      <ProjectsSection />
+      <Team />
+      <TechStack />
+      <Timeline />
+      <Footer />
+    </Fragment>
+  );
+}
