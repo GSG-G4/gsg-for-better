61fc2ae3d45e4087117038b4488cd3c00e2b1187
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-28 22:31:34 +0200

    feat: add propTypes validation Relates #42

--- client/src/components/LandingPage/Team/TeamCard.js
+++ client/src/components/LandingPage/Team/TeamCard.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default (teamMemberInfo) => (
+import {
+  CardWrapper,
+  ImageContainer,
+  CardImage,
+  UserName,
+  UserSpecialty,
+} from './StyledCompnents';
 
+import CardImageBackground from './team_card.png';
+
+const TeamCard = ({ name, profileImage, specialty }) => (
+  <CardWrapper>
+    <ImageContainer img={CardImageBackground}>
+      <CardImage src={profileImage} />
+    </ImageContainer>
+    <UserName>{name}</UserName>
+    <UserSpecialty>{specialty}</UserSpecialty>
+  </CardWrapper>
 );
+
+export default TeamCard;
+
+TeamCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  profileImage: PropTypes.string.isRequired,
+  specialty: PropTypes.string.isRequired,
+};
