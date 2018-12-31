f5ea1b14cdbb676d0c91def31f3033baeea7a568
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-31 17:45:03 +0200

    feat: add redux-thunk as middleware Relates #43

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import appReducer from '../reducers';
 
 const initialState = {};
 
-const store = createStore(appReducer, initialState);
+const middlewares = [thunk];
+
+const store = createStore(
+  appReducer,
+  initialState,
+  applyMiddleware(...middlewares)
+);
 
 export default store;
