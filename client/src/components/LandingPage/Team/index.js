0890753d7972576df96699ccd1dd547642539c0f
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-31 23:43:31 +0200

    feat: add prop types validation Relates #43

--- client/src/components/LandingPage/Team/index.js
+++ client/src/components/LandingPage/Team/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import fetchTeamMembersAction from '../../../actions/fetch_team_members';
 
@@ -19,10 +20,31 @@ class Team extends Component {
   state = {
     teamMembers: [],
   };
+  // githubHandler: 'lubnaabd',
+  // email: 'lubnaabd@hotmail.com',
+  // role: 'member',
+  // profileImage: 'https://avatars1.githubusercontent.com/u/26024284?s=460&v=4',
+  // team: team2,
+  // specialty: 'full-stack web developer',
+
+  static propTypes = {
+    fetchTeamMembers: PropTypes.func.isRequired,
+    teamId: PropTypes.string.isRequired,
+    teamMembers: PropTypes.arrayOf(
+      PropTypes.shape({
+        githubHandler: PropTypes.string,
+        email: PropTypes.string,
+        role: PropTypes.string,
+        profileImage: PropTypes.string,
+        team: PropTypes.string,
+        specialty: PropTypes.string,
+      }).isRequired
+    ).isRequired,
+  };
 
   componentDidMount() {
-    const { fetchTeamMembers } = this.props;
-    fetchTeamMembers(111);
+    const { fetchTeamMembers, teamId } = this.props;
+    fetchTeamMembers(teamId);
   }
 
   componentDidUpdate = prevProps => {
@@ -34,13 +56,23 @@ class Team extends Component {
   };
 
   render() {
+    const { teamMembers } = this.state;
     return (
       <TeamWrapper>
         <Header>
           <HeaderImage src={TeamHeaderImage} />
           <Heading>Who We Are</Heading>
         </Header>
-        <TeamCardsWrapper />
+        <TeamCardsWrapper>
+          {teamMembers.map(member => (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              profileImage={member.profileImage}
+              specialty={member.specialty}
+            />
+          ))}
+        </TeamCardsWrapper>
       </TeamWrapper>
     );
   }
